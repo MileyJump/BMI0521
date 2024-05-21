810ccf009bb5041697baad0780cbd9f12def44c3
choiminkyung <choialsrud2002@gmail.com>
2024-05-21 16:08:09 +0900

    Initial Commit

---
+++ BMI0521/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI0521
+//
+//  Created by 최민경 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
